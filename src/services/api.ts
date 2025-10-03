db0c8b086524b6877b8130d8acbb26cff0575308
Ogboi Favour <ogboifavourifeanyichukwu@gmail.com>
2025-10-03 14:28:10 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ import {
 } from '../types/index';
 
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://sms-guard-backend.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
